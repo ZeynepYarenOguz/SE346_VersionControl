410312d2be262ea691a860c8f83adece78154a0a
NuriCanOzer <74374791+NuriCanOzer@users.noreply.github.com>
2022-05-14 22:16:47 +0300

    v1.9

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -28,4 +28,5 @@ int main()
 	printf("Kağan Gökbayrak,18243610030,ISE\n");
 	printf("Enes Furkan Akilli,18243610022,ISE\n");
 	printf("Hüseyin Emre Kaya,18243610035,ISE\n");
+	printf("Nuri Can Ozer,18243610027,ISE\n");
 }
