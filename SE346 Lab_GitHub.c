7b89e61ad45d82abaa5795366404880f3a7df5dd
berilcakutlu <kutlu.berilcan@student.atilim.edu.tr>
2022-05-14 20:23:17 +0300

    v2.6

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -14,4 +14,6 @@ int main()
 	printf("Mehmet Önder Peker, 17243510057, CMPE\n");
 	printf("Ece Demirel,18243510017,CMPE\n");
 	printf("Altan Samet Aldeniz, 19243510015, CMPE\n");
+	printf("Poyraz Yilan,19243510012,CMPE\n");
+	printf("Berilcan Kutlu,150302018,CMPE\n");
 }
