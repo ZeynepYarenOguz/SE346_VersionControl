1f3e394d0a24185d1e6249a008e17bdca53c819a
tugberkozsen <tugberkozsen@outlook.com>
2022-05-15 00:36:53 +0300

    v1.2

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -8,26 +8,7 @@ int main()
 
 	printf("Z.Yaren Oguz,123456,SE\n");
 
-	printf("İsmail Yıldırım, 17243610019, ISE\n");
-	printf("Berna Nur UNAL,19243610040,ISE\n");
-	printf("Nazlı Melike Kurtoğlu, 18243510053, CMPE\n");
-	printf("Roşna Nur Sarıkaş, 18243510047, CMPE\n");
-	printf("Mehmet Önder Peker, 17243510057, CMPE\n");
-	printf("Ece Demirel,18243510017,CMPE\n");
-	printf("Altan Samet Aldeniz, 19243510015, CMPE\n");
-	printf("Poyraz Yilan,19243510012,CMPE\n");
-	printf("Berilcan Kutlu,150302018,CMPE\n");
-	printf("Övünç Can Durukan,19243510069,CMPE\n");
-	printf("Orhun Arda Kaya,18243510023,CMPE\n");
-	printf("Aysu İrem Adem,18243510031,CMPE\n");
-	printf("Mahmut Furkan Bakal,18243510019,CMPE\n");
-	printf("Göksu Can Çiftçi, 20243510083, CMPE\n");
-	printf("Serife Gozde Mesci, 18243510015, CMPE");
-	printf("Kutay Akkaya, 18243510025, CMPE");
-	printf("Aleyna KIZILTAŞ,17243610014,ISE\n");
-	printf("Kağan Gökbayrak,18243610030,ISE\n");
-	printf("Enes Furkan Akilli,18243610022,ISE\n");
-	printf("Hüseyin Emre Kaya,18243610035,ISE\n");
-	printf("Nuri Can Ozer,18243610027,ISE\n");
-	printf("Abdullah Erol, 18243510001, CMPE\n");
+	printf("Ozge Tekin,987654,SE\n");
+
+	printf("Tugberk Ozsen,17243510052,CMPE\n");
 }
