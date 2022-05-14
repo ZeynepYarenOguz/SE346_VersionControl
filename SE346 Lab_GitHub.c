5c7e669fc54578830c9c6fdfa8faec358fcfede9
mehmetonderpeker <mehmetonderpeker@gmail.com>
2022-05-14 18:35:59 +0300

    v1.8

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -11,4 +11,5 @@ int main()
     printf("Berna Nur UNAL,19243610040,ISE\n");
 	printf("Nazlı Melike Kurtoğlu, 18243510053, CMPE\n");
 	printf("Roşna Nur Sarıkaş, 18243510047, CMPE\n");
+	printf("Mehmet Önder Peker, 17243510057, CMPE\n");
 }
